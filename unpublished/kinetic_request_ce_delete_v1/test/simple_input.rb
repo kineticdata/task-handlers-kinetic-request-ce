0c8a4cacc82078c47e79a0e4ba30967d45a7114d
anneramey <anne.ramey@kineticdata.com>
2018-06-04 11:53:29 -0500

    Initial Version of Generic CE Handlers
    These are intended for use with the import/export service item

---
+++ unpublished/kinetic_request_ce_delete_v1/test/simple_input.rb
@@ -0,0 +1,16 @@
+{
+  'info' => {
+    'api_server' => 'https://kinops.io',
+    'api_username' => '',
+    'api_password' => '',
+    'space_slug' => '',
+    'enable_debug_logging' => 'Yes'
+  },
+  'parameters' => {
+    'error_handling' => 'Error Message',
+    'space_slug' => '',
+    'type' => 'form',
+    'kapp' => 'services',
+    'slug' => 'cleaning2'
+  }
+}
---
+++ unpublished/kinetic_request_ce_retrieve_v1/test/simple_input.rb
@@ -0,0 +1,17 @@
+{
+  'info' => {
+    'api_server' => 'https://kinops.io',
+    'api_username' => '',
+    'api_password' => '',
+    'space_slug' => '',
+    'enable_debug_logging' => 'Yes'
+  },
+  'parameters' => {
+    'error_handling' => 'Error Message',
+    'space_slug' => '',
+    'type' => 'forms',
+    'kapp' => 'catalog',
+    'slug' => 'cleaning',
+    'includes' => 'detail,fields'
+  }
+}
