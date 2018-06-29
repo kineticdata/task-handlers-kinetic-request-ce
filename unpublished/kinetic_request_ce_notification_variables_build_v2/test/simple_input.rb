a3c92b897f5e5580b101f0c40491ba7f3d05ad25
mhowe <matthew.howe@kineticdata.com>
2018-06-29 14:29:54 -0500

    Created Notification_Variables_Build_v2 handler
    Handler now deals with datastore submissions.

---
+++ unpublished/kinetic_request_ce_notification_variables_build_v2/test/simple_input.rb
@@ -0,0 +1,20 @@
+{
+  'info' => {
+    'api_server' => 'https:/yourserver.com/kinetic',
+    'api_username' => '',
+    'api_password' => '',
+    'space_slug' => '',
+    'enable_debug_logging'=>'yes'
+  },
+  'parameters' => {
+    'error_handling' => 'Error Message',
+    'space_slug' => '',
+    'is_datastore' => 'No',
+    'kapp_slug' => '',
+    'form_slug' => '',
+    'submission_id' => '',
+    'username' => '',
+    'addt_vars' => '{"testAddtlVar-NAME":"testAddtlVar-VALUE"}',
+    'backups' => '{"spaceAttributes":{"testAttribute":"testAttributeValue"}}',
+  }
+}
