63e7a8922a312cf2df3c99c720f28a1fa09e70b3
mhowe <matthew.howe@kineticdata.com>
2019-01-14 13:17:57 -0600

    Added in all API options as parameters
    Previous version of the handler didn't support all of the API options.

---
+++ published/kinetic_request_ce_submission_search_v2/test/simple_input.rb
@@ -0,0 +1,25 @@
+{
+  'info' => {
+    'api_server' => 'https://localhost:8080/kinetic',
+    'api_username' => 'user@kineticdata.com',
+    'api_password' => 'password',
+    'space_slug' => '',
+    'enable_debug_logging'=>'yes'
+  },
+  'parameters' => {
+    'error_handling' => 'Error Message',
+    'space_slug' => '',
+    'kapp_slug' => 'services',
+    'form_slug' => 'cleaning',
+    'include' => 'details,values',
+    'core_state' => '',
+    'timeline' => '',
+    'direction' => '',
+    'start' => '',
+    'end' => '',
+    'limit' => '1000',
+    'query' => 'values[Name] = "Mark"',
+    'page_token' => '',
+    'return_type' => 'JSON'
+  }
+}
