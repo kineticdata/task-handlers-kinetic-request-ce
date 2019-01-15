43c6966f58bd2b83c71d3239a0040b8a5a522bf9
Chad Rehm <chad.rehm@kineticdata.com>
2019-01-15 09:09:43 -0600

    PER-129 Initial commit of team delete handler

---
+++ unpublished/kinetic_request_ce_team_delete_v1/test/simple_input.rb
@@ -0,0 +1,14 @@
+{
+  'info' => {
+    'api_server' => '',
+    'api_username' => '',
+    'api_password' => '',
+    'space_slug' => '',
+    'enable_debug_logging' => 'true'
+  },
+  'parameters' => {
+    'error_handling' => 'Error Message',
+	  'space_slug' => '',
+    'team_slug' => 'fedda3bb97792af27c00ac861b13ee32',
+  }
+}
